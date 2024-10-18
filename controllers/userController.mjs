248e19f09e12291f24c7cad6dbb1347a6075905b
Faizan Ahmad Khan <fa3n20004@gmail.com>
2024-10-18 21:43:48 +0500

    get request added

---
+++ controllers/userController.mjs
@@ -0,0 +1,11 @@
+import User from "../models/User.mjs";
+
+// Get all users
+export const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json({ message: "Error fetching users" });
+  }
+};
